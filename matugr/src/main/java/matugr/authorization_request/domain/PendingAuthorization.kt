8e750c59917b01ef7d9a5174ef8e149bab71a9d0
Jude Pinto <judepinto2022@gmail.com>
2022-02-13 14:07:45 -0800

    adding comments for pending authorization store

--- matugr/src/main/java/matugr/authorization_request/domain/PendingAuthorization.kt
+++ matugr/src/main/java/matugr/authorization_request/domain/PendingAuthorization.kt
@@ -3,6 +3,9 @@ package matugr.authorization_request.domain
 import kotlinx.coroutines.CancellableContinuation
 import matugr.authorization_request.external.AuthorizationResult
 
+/**
+ * Data holder for information needed to process authorization response received by app
+ */
 internal data class PendingAuthorization(
     val authorizationContinuation: CancellableContinuation<AuthorizationResult>,
     val codeVerifier: String,
--- matugr/src/main/java/matugr/authorization_request/domain/PendingAuthorizationStore.kt
+++ matugr/src/main/java/matugr/authorization_request/domain/PendingAuthorizationStore.kt
@@ -1,5 +1,8 @@
 package matugr.authorization_request.domain
 
+/**
+ * API for authorization request logic to interact with authorization response logic.
+ */
 internal interface PendingAuthorizationStore {
     fun storePendingAuthorization(pendingAuthorization: PendingAuthorization)
     fun cancelPendingAuthorization()
